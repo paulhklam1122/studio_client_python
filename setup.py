c895c20a40b11b73286c395b32df68c1dce7ab67
2lives <brandon@skylabtech.ai>
2023-10-23 14:58:39 -0700

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as fp:
 
 setup(
     name='skylab_studio',
-    version='0.0.7',
+    version='0.0.8',
     author='skylabtech',
     author_email='info@skylabtech.ai',
     packages=find_packages(),
